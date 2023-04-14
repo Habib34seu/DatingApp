6bf9a00d38c97867f277620de6201781f2ad2e74
Habib34seu <34seu.habib@gmail.com>
2023-04-15 01:41:07 +0600

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -12,5 +8,21 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] Passwordsalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; }  = DateTime.UtcNow;
+        public string  Gander { get; set; }
+        public string Introduction { get; set; }
+        public string LokingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new List<Photo>();
+
+        // public int GetAge(){
+        //     return DateOfBirth.CalculateAge();
+        // }
+
     }
 }
